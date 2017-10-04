139414e52abd1c3aa8c41ab4e5859e30efc052ec
Thomas Buchberger <t.buchberger@4teamwork.ch>
2017-10-04 11:26:04 +0200

    Avoid adding styles multiple times

--- docxcompose/composer.py
+++ docxcompose/composer.py
@@ -120,8 +120,8 @@ class Composer(object):
     def add_styles(self, doc, element):
         """Add styles from the given document used in the given element."""
         our_style_ids = [s.style_id for s in self.doc.styles]
-        used_style_ids = [e.val for e in xpath(
-            element, './/w:tblStyle|.//w:pStyle|.//w:rStyle')]
+        used_style_ids = set([e.val for e in xpath(
+            element, './/w:tblStyle|.//w:pStyle|.//w:rStyle')])
         for style_id in used_style_ids:
             if style_id not in our_style_ids:
                 style_element = doc.styles.element.get_by_id(style_id)
