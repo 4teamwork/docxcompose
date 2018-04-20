2a27e7313f67f8ff8368e590de0d719cee515ff7
Thomas Buchberger <t.buchberger@4teamwork.ch>
2018-04-20 11:42:26 +0200

    Preparing release 1.0.0a7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ tests_require = [
 
 setup(
     name='docxcompose',
-    version='1.0.0a7.dev0',
+    version='1.0.0a7',
     description="Compose .docx documents",
     long_description=(open("README.rst").read() + "\n" +
                       open("HISTORY.txt").read()),
