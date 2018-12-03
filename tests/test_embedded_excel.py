05dab068008a3e3ac1cc3193d575581f275ce156
Thomas Buchberger <t.buchberger@4teamwork.ch>
2018-12-03 18:15:13 +0100

    Add test with embedded Excel chart

---
+++ tests/test_embedded_excel.py
@@ -0,0 +1,10 @@
+from utils import ComposedDocument
+from utils import FixtureDocument
+
+
+def test_hyperlinks():
+    doc = FixtureDocument("embedded_excel_chart.docx")
+    composed = ComposedDocument(
+        "master.docx", "embedded_excel_chart.docx")
+
+    assert composed == doc
