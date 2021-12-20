1584b79c7ba6ef1c7477de2615c6564e11667b06
Lukas Graf <lukas.graf@4teamwork.ch>
2021-12-20 09:55:23 +0100

    Back to development: 1.3.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ tests_require = [
 
 setup(
     name='docxcompose',
-    version='1.3.4',
+    version='1.3.5.dev0',
     description="Compose .docx documents",
     long_description=(open("README.rst").read() + "\n" +
                       open("HISTORY.txt").read()),
