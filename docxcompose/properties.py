5499748cb026fa5009659d812f4f4251195614e0
Pierpaolo Da Fieno <pierpaolo.dafieno@gmail.com>
2025-07-09 16:30:28 +0200

    Fixed DeprecationWarning on pkg_import.

--- docxcompose/properties.py
+++ docxcompose/properties.py
@@ -15,7 +15,8 @@ from lxml.etree import FunctionNamespace
 from lxml.etree import QName
 from six import binary_type
 from six import text_type
-import pkg_resources
+#import pkg_resources
+import importlib_resources
 import re
 
 
@@ -108,8 +109,10 @@ class CustomProperties(object):
             self._element = parse_xml(part.blob)
 
     def _part_template(self):
-        return pkg_resources.resource_string(
-            'docxcompose', 'templates/custom.xml')
+        # return pkg_resources.resource_string(
+        #    'docxcompose', 'templates/custom.xml')
+        ref = importlib_resources.files('docxcompose').joinpath('templates/custom.xml')
+        return ref.read_bytes()
 
     def _update_part(self):
         if self.part is None:
